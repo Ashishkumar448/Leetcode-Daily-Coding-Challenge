4926487e9cdbafaa1b9eedd458def48fb0cd44a6
Ashishkumar448 <165567553+Ashishkumar448@users.noreply.github.com>
2024-08-13 09:14:21 +0530

    Create Combination sum ii.py

---
+++ 2024-08 August- Leetcoding challenge/Combination sum ii.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(start, path, target):
+            if target == 0:
+                res.append(path)
+                return
+            if target < 0:
+                return
+            
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                backtrack(i + 1, path + [candidates[i]], target - candidates[i])
+        
+        candidates.sort()
+        res = []
+        backtrack(0, [], target)
+        return res
